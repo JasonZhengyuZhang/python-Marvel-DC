ca753eadacc315e125b9b3a5d5bbd7ab6ffb0c60
ZhengyuZhang <zhengyuhood@gmail.com>
2018-08-13 16:34:54 -0400

    gamemode and hero selection

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import time 
+
 class Superhero:
     def __init__(self, name, franchise, health, attack):
         self.name=name
@@ -23,8 +26,37 @@ class SpecialCard:
         self.probability=probability
 
 Shazam = Superhero("Shazam", "DC", 5000, 2000)
+IronMan = Superhero("Iron Man", "Marvel", 4000, 3000)
+CaptainAmerica = Superhero("Captain America", "Marvel", 3000, 3000)
+
+
+HeroList = [Shazam, IronMan, CaptainAmerica]
+
+def countdown(t):
+    while t:
+        print(str(t) + ", ")
+        time.sleep(1)
+        t-=1
+
+def startGamemode1(playerName):
+    print("\n" + "A hero will be selected for you ")
+    countdown(10)
+
+    playerHero = random.choice(HeroList)
+    print("Your hero is " + playerHero.name)
 
 def startGame():
     print("\n\n"+"Welcome to the battle!")
+    playerName=raw_input("Hero, please enter your name: ")
+
+    gamemode = 0
+
+    while gamemode != 1:
+        gamemode=input("\n" + "Which gamemode would you like to play: ")
+        print(gamemode)
+
+    if gamemode == 1:
+        startGamemode1(playerName)
+
 
 startGame()
