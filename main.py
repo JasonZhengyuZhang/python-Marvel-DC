6cffbca27834ebd61a9d348de138301e3caa2e6c
ZhengyuZhang <zhengyuhood@gmail.com>
2018-08-15 15:32:51 -0400

    draw per round mech

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def initialCardDeal():
     
     time.sleep(1)
 
-    print("\n Deck are being split... \n")
+    print("\nDeck are being split...")
+    time.sleep(1)
     
     while len(playerDeckList) != 4 or len(opponentDeckList) != 4:
         currentRoundPlayerCardType = random.choice(fullDeckList)
@@ -48,6 +49,17 @@ def initialCardDeal():
 
     initalCard = [playerDeckList, opponentDeckList]
     return initalCard
+
+def drawCard():
+    fullDeckList = []
+    fullDeckList.append(AttackCardList)
+    fullDeckList.append(HealCardList)
+    fullDeckList.append(SpecialCardList)
+
+    currentRoundCardType = random.choice(fullDeckList)
+    currentRoundCard = random.choice(currentRoundCardType)
+
+    return [currentRoundCard.getName(), currentRoundCard]
 
 def DeckSort(deck):
     availableHeal = []
@@ -124,19 +136,35 @@ def startGamemode1(playerName):
     turn = random.choice([1,2])
     turnDict = {"player": False, "computer": False}
 
+    time.sleep(1)
     if turn==0:
         turnDict["player"]=True
         print("A dice was tossed and you will go first")
     else:
         turnDict["computer"]=True
         print("A dice was tossed and the computer will go first")
+    time.sleep(1)
 
-    while player.health > 0 or computer.health > 0:
+    while player.health > 0 and computer.health > 0:
         if turnDict["player"]==True:
+            print("\nIt is your turn")
+            time.sleep(1)
+            newCard = drawCard()
+
+            if newCard[0] == "AttackCard":
+                player.attackCard.append(newCard[1])
+            elif newCard[0] == "HealCard":
+                player.healCard.append(newCard[1])
+            else:
+                player.specialCard.append(newCard[1])
+
+            print("You have drawn your card and below is your current deck")
+            time.sleep(1)
+            deckStatus(player.attackCard, player.healCard, player.specialCard)
+
             computer.health-=1000
-            print("players turn")
         else:
-            print("computer's turn")
+            print("\ncomputer's turn")
 
         turnDict["player"] = not turnDict["player"]
         turnDict["computer"] = not turnDict["computer"]
